dcc5526e6654c9588676d11e751194c04a0b100f
Nipun Anupama <anupamaalahakoon66@gmail.com>
2025-02-20 20:29:49 +0530

    add task

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -8,28 +8,76 @@ function App() {
     title: "",
     date: "",
     state: "",
-    priority: "",
+    priority: "false",
   });
+
+  const [taskList, setTaskList] = useState([]);
 
   console.log(task);
 
   const handleOnChange = (e) => {
-    setTask({ ...task, [e.target.name]: e.target.value });
+    if (e.target.name == "priority") {
+      setTask({ ...task, [e.target.name]: e.target.checked });
+    } else {
+      setTask({ ...task, [e.target.name]: e.target.value });
+    }
+
   };
+
+  const handleAdd = () => {
+    // const sampleList = [...taskList];
+    // sampleList.push(task);
+    // setTaskList(sampleList); // this method is use js this tow option are same output
+
+    setTaskList([...taskList, task]);  // React is use best
+  };
+
 
   return (
     <div className='App'>
-      <div className='h-screen w-auto bg-blue-200'>
-        <div className='flex items-center justify-between'>
+      <div className='h-screen w-auto bg-blue-200 px-6'>
+        <div className='flex items-center gap-8'>
           <div className='flex flex-col'>
             <label>Task</label>
             <input
               name="title"
+              type='text'
               onChange={(e) => handleOnChange(e)}
               value={task.title}
               placeholder='task'
             />
           </div>
+          <div className='flex flex-col'>
+            <label>Date</label>
+            <input
+              name="date"
+              type='date'
+              onChange={(e) => handleOnChange(e)}
+              value={task.date}
+              placeholder='12/12/2021'
+            />
+          </div>
+          <div className='flex flex-col'>
+            <label>Priority</label>
+            <input
+              name="priority"
+              type='checkbox'
+              onChange={(e) => handleOnChange(e)}
+              checked={task.priority}
+            />
+          </div>
+          <div>
+            <button className='bg-blue-600 px-4 py-2 min-w-[6rem] rounded-2xl' onClick={() => handleAdd()}>Add</button>
+          </div>
+        </div>
+        <div className="mt-4">
+          {taskList.map((tasks, idx) => (
+            <div key={idx}>
+              <div>{tasks.title}</div>
+              <div>{tasks.date}</div>
+              <div>{tasks.priority}</div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
