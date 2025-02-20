958911fb18dc92fcf5c888d6569fed86c6eb1c1d
Nipun Anupama <anupamaalahakoon66@gmail.com>
2025-02-20 17:38:16 +0530

    tailwind add

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,16 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import "tailwindcss";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [task, setTask] = useState({
+    title: "",
+    date: "",
+    state: "",
+    priority: "",
+  });
+
+  console.log(task);
+
+  const handleOnChange = (e) => {
+    setTask({ ...task, [e.target.name]: e.target.value });
+  };
 
   return (
-    <>
-      <p>Hello</p>
-    </>
-  )
+    <div className='App'>
+      <div className='h-screen w-auto bg-blue-200'>
+        <div className='flex items-center justify-between'>
+          <div className='flex flex-col'>
+            <label>Task</label>
+            <input
+              name="title"
+              onChange={(e) => handleOnChange(e)}
+              value={task.title}
+              placeholder='task'
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App
